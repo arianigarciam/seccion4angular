be15e468349b151150f3981beafea37120469dc7
arianigarciam <arianigarciam@gmail.com>
2021-03-07 14:55:56 -0400

    Clase 38

--- bases/src/app/app.component.ts
+++ bases/src/app/app.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title: string = 'bases';
+  title: string = 'bases'; 
   numero:number = 10 ;
+  base: number = 5;
 
   acumular(valor : number ){
     this.numero += valor;

3e4af494ab47fd58e08a8144bba9118debb80acf
arianigarciam <arianigarciam@gmail.com>
2021-03-07 16:03:48 -0400

    Clase 41

--- bases/src/app/heroes/heroe/heroe.component.ts
+++ bases/src/app/heroes/heroe/heroe.component.ts
@@ -7,5 +7,16 @@ import { Component } from "@angular/core";
 
 
 export class HeroeComponent {
+    nombre: string = 'Ironman';
+    edad: number = 45;
+
+    get nombreCapitalizado () {
+        return this.nombre.toLocaleUpperCase();
+    }
+
+    obtenerNombre() :string {
+        return `${this.nombre } - ${this.edad}
+        `;
+    }
 
 }
